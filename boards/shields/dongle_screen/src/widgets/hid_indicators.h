ecf65404e853825d19c91f0e20ec5121b5fca4e3
Tony Fischer (tku137) <tku137@mailbox.org>
2026-02-06 22:05:19 +0100

    feat: add caps_word widget

--- boards/shields/dongle_screen/src/widgets/hid_indicators.h
+++ boards/shields/dongle_screen/src/widgets/hid_indicators.h
@@ -17,6 +17,8 @@ struct zmk_widget_hid_indicators {
     lv_obj_t *num_icon;
     lv_obj_t *scroll_label;
     lv_obj_t *scroll_icon;
+    lv_obj_t *caps_word_icon;
+    lv_obj_t *caps_word_label;
     sys_snode_t node;
 };
 
